a7b181f20ec983c99fa5f50424b632922436db34
javiscripto <javier.mecker94@gmail.com>
2024-07-10 16:59:15 -0400

    agrego el campo displayName a la tabla sessions

--- DB/index.js
+++ DB/index.js
@@ -5,20 +5,21 @@ let db = null
 export const init = async () => {
   db = await SQLite.openDatabaseAsync('sessions.db')
   await db.execAsync(
-    'CREATE TABLE IF NOT EXISTS sessions (localId TEXT PRIMARY KEY NOT NULL, email TEXT NOT NULL, token TEXT NOT NULL)'
+    'CREATE TABLE IF NOT EXISTS sessions (localId TEXT PRIMARY KEY NOT NULL, email TEXT NOT NULL, token TEXT NOT NULL, displayName TEXT NOT NULL)'
   )
 }
 
-export const insertSession = async ({ email, localId, idToken }) => {
+export const insertSession = async ({ email, localId, idToken, displayName }) => {
   try {
     await db.runAsync(
-      'INSERT INTO sessions (localId, email, token) VALUES (?, ?, ?)',
+      'INSERT INTO sessions (localId, email, token, displayName) VALUES (?, ?, ?,?)',
       localId,
       email,
-      idToken
+      idToken,
+      displayName
     )
   } catch (error) {
-    console.error('Error insertando sesión:', error)
+    console.error('db: Error insertando sesión:', error)
   }
 }
 
@@ -32,5 +33,6 @@ export const deleteSession = async () => {
 
 export const fetchSession = async () => {
   const firstRow = await db.getFirstAsync('SELECT * FROM sessions')
+  console.log("data : ", firstRow)
   return firstRow
 }
