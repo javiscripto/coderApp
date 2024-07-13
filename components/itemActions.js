7a681aaef14f773c339d348a1078ec7bd465d9af
javiscripto <javier.mecker94@gmail.com>
2024-07-12 22:12:07 -0400

    implementacion de useAddWishListitemMutation

--- components/itemActions.js
+++ components/itemActions.js
@@ -1,58 +1,62 @@
-import React, {  useState } from 'react';
+import React, { useState } from 'react';
 import { Pressable, StyleSheet, View, Text, Alert } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-
-import CustomToast from './alerts/customToast';
-
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addProduct } from '../features/cartSlice';
 import { CustomButton } from './customButton';
+import CustomToast from './alerts/customToast';
+import { useAddWishListItemMutation } from '../services/shopService';
+import Loader from './loader';
 
-export default function ItemActions({product}) {
-
+export default function ItemActions({ product }) {
     const [toastVisible, setToastVisible] = useState(false);
     const [toastMessage, setToastMessage] = useState('');
     const [isFavorite, setIsFavorite] = useState(false);
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
+    const { localId } = useSelector(state => state.auth.value.user);
 
-
+    const [triggerAddItem, { isLoading, isError, data }] = useAddWishListItemMutation();
 
     const showToast = (message) => {
         setToastMessage(message);
         setToastVisible(true);
     };
 
-    const handleFavoritePress = () => {
-        setIsFavorite(prevState => !prevState);
-        showToast(`Se ha ${isFavorite ? 'removido' : 'agregado'} ${product.brand} de favoritos`);
+    const handleFavoritePress = async () => {
+        try {
+            
+            await triggerAddItem({ localId, product });
+
+            setIsFavorite(prevState => !prevState);
+            showToast(`Se ha ${isFavorite ? 'removido' : 'agregado'} ${product.brand} de favoritos`);
+        } catch (error) {
+            console.error("Error adding to wishlist:", error);
+            showToast("Hubo un error al agregar el producto a favoritos");
+        }
     };
 
-    const handleAddToCartPress =()=>{
-        dispatch(addProduct(product))
-        showToast(`Se ha agregado ${product.brand} al carrito`)
-    }
+    const handleAddToCartPress = () => {
+        dispatch(addProduct(product));
+        showToast(`Se ha agregado ${product.brand} al carrito`);
+    };
 
     return (
         <View style={styles.itemWidget}>
+            {isLoading&&(<Loader/>)}
             <View style={styles.secondaryButtons}>
                 <Pressable onPress={handleFavoritePress}>
-                
                     <FontAwesome5 name="bolt" size={24} color={isFavorite ? "black" : "grey"} />
                 </Pressable>
                 <Pressable onPress={() => Alert.alert("Compartir")}>
-
                     <AntDesign name="sharealt" size={24} color="black" />
                 </Pressable>
             </View>
 
-
-          <CustomButton onPress={handleAddToCartPress}>
-            Agregar al carrito
-          </CustomButton>
-
-
+            <CustomButton onPress={handleAddToCartPress}>
+                Agregar al carrito
+            </CustomButton>
 
             <CustomToast
                 visible={toastVisible}
